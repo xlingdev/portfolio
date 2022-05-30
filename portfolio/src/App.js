ac22a59e6b371e6405c51264e3a24154c6ce5e3b
Knox Lingenfelter <43417409+xlingdev@users.noreply.github.com>
2022-05-30 18:54:12 -0500

    Nice

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Knox Lingenfelterß
+          Knox Lingenfelter
         </p>
         <a
           className="App-link"
