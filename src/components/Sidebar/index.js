bfc70c60cd62385bc1a5e4dbf3044ef7fe81b90c
Knox Lingenfelter <43417409+xlingdev@users.noreply.github.com>
2022-05-30 20:16:29 -0500

    asd

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,4 @@
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faLinkedin,
