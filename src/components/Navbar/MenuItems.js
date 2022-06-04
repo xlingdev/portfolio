41e5e88c9840934ff994fe833a0a316af9dd3f7f
Knox Lingenfelter <43417409+xlingdev@users.noreply.github.com>
2022-06-04 10:34:20 -0500

    yea

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Portfolio',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Links',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links'
+    },
+]
